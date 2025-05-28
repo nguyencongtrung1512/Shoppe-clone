2bee95ed960645905d8c17e34a5c37cc8a9f0643
nguyencongtrung1512 <trungtrun37a@gmail.com>
2025-05-28 15:48:24 +0700

    Slider zoom for ProductDetail

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -4,6 +4,7 @@ import productApi from '../../apis/product.api'
 import { formatCurrency, formatNumberToSocialStyle, rateSale } from '../../utils/utils'
 import InputNumber from '../../component/inputNumber'
 import DOMPurify from 'dompurify'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 function ProductDetail() {
   const { id } = useParams()
@@ -11,24 +12,89 @@ function ProductDetail() {
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
+  const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [activeImages, setActiveImages] = useState('')
   const product = productDetailData?.data.data
-  console.log(product)
+  const currentImages = useMemo(
+    () => (product ? product?.images.slice(...currentIndexImages) : []),
+    [product, currentIndexImages]
+  )
+  const imageRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (product && product?.images.length > 0) {
+      setActiveImages(product?.images[0])
+    }
+  }, [product])
+
+  const choseImages = (img) => {
+    setActiveImages(img)
+  }
+
+  const nextImages = () => {
+    if (currentIndexImages[1] < product?.images.length) {
+      setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+  const prevImages = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+  const handleZoom = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = event.currentTarget.getBoundingClientRect()
+    const image = imageRef.current as HTMLImageElement
+    const { naturalWidth, naturalHeight } = image
+    // cachs 1: lấy offsetX, offsetY đươn giản khi chúng ta xử lí được bubble event bằng cách thêm pointer-event-none vào class 
+    //const { offsetX, offsetY } = event.nativeEvent
+    //cách 2: Lấy offsetX, offsetY khi chúng ta không xử lý event bubble
+    const offsetX = event.pageX - (rect.x + window.scrollX)
+    const offsetY = event.pageY - (rect.y + window.scrollY)
+
+    const top = offsetY * (1 - naturalWidth / rect.width)
+    const left = offsetX * (1 - naturalHeight / rect.height)
+    image.style.width = naturalWidth + 'px'
+    image.style.height = naturalHeight + 'px'
+    image.style.maxWidth = 'unset'
+    image.style.top = top + 'px'
+    image.style.left = left + 'px'
+
+    //event bubble
+  }
+
+  const handleRemoveZoom = () => {
+    const image = imageRef.current as HTMLImageElement
+    image.style.width = '100%'
+    image.style.height = '100%'
+    image.style.maxWidth = 'unset'
+    image.style.top = '0'
+    image.style.left = '0'
+  }
+  // console.log(product)
   if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='mt-8 bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
-              <div className='relative w-full pt-[100%] shadow'>
+              <div
+                className='relative w-full overflow-hidden pt-[100%] shadow cursor-zoom-in'
+                onMouseMove={handleZoom}
+                onMouseLeave={handleRemoveZoom}
+              >
                 <img
-                  src={product.image}
+                  src={activeImages}
                   alt={product.name}
+                  ref={imageRef}
                   className='absolute top-0 left-0 w-full h-full object-cover bg-white'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={prevImages}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -40,12 +106,12 @@ function ProductDetail() {
                     <path stroke-linecap='round' stroke-linejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
                   </svg>
                 </button>
-                {product.images.slice(0, 5).map((img: any, index) => {
-                  const isActive = index === 0
+                {currentImages.map((img) => {
+                  const isActive = img === activeImages
                   return (
-                    <div className='relative w-full pt-[100%]' key={img}>
+                    <div className='relative w-full pt-[100%]' key={img} onMouseEnter={() => choseImages(img)}>
                       <img
-                        src={product.image}
+                        src={img}
                         alt={product.name}
                         className='absolute top-0 left-0 w-full h-full object-cover bg-white cursor-pointer object-contain'
                       />
@@ -53,7 +119,10 @@ function ProductDetail() {
                     </div>
                   )
                 })}
-                <button className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={nextImages}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -129,7 +198,7 @@ function ProductDetail() {
                     className=''
                     classnameError='hidden'
                     classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
-                    onChange={() => { }}
+                    onChange={() => {}}
                   />
                   <button className='flex h-8 w-8 items-center justify-center border border-gray-300 text-gray-600'>
                     <svg
@@ -173,8 +242,8 @@ function ProductDetail() {
               </div>
             </div>
           </div>
-          <div className='mt-8 bg-white p-4 shadow'>
-            <div className='container'>
+          <div className='container'>
+            <div className='mt-8 bg-white p-4 shadow'>
               <h2 className='text-lg font-medium uppercase rounded text-slate-700'>Mô tả sản phẩm</h2>
               <div className='mt-6 text-sm leading-loose text-gray-600'>
                 <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(product.description) }} />
